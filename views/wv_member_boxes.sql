cad49ce05992aec970b9348a5df88f680b46507d
dpslwk <dps.lwk@gmail.com>
2019-01-30 22:12:23 +0000

    Default for members.join_date and update members_box view

--- views/wv_member_boxes.sql
+++ views/wv_member_boxes.sql
@@ -1,6 +1,73 @@
 DROP VIEW IF EXISTS `wv_member_boxes`;
 
 CREATE VIEW `wv_member_boxes` AS
+
+SELECT
+   -(1) AS member_box_id,
+   count(*) AS user_id,
+   '' AS member_name,
+   '' AS bought_date,
+   'In Use' AS box_status,
+   'Any' AS member_status
+FROM member_boxes
+WHERE (member_boxes.state = 10)
+
+UNION ALL
+
+SELECT
+   -(1) AS member_box_id,
+   count(*) AS user_id,
+   '' AS member_name,
+   '' AS bought_date,
+   'Removed' AS box_status,
+   'Any' AS member_status
+FROM member_boxes
+WHERE (member_boxes.state = 20)
+
+UNION ALL
+
+SELECT
+   -(1) AS member_box_id,
+   count(*) AS user_id,
+   '' AS member_name,
+   '' AS bought_date,
+   'Abandoned' AS box_status,
+   'Any' AS member_status
+FROM member_boxes
+WHERE (member_boxes.state = 30)
+
+UNION ALL
+
+SELECT
+   -(1) AS member_box_id,
+   count(*) AS user_id,
+   '' AS member_name,
+   '' AS bought_date,
+   'In Use' AS box_status,
+   status.title AS member_status
+FROM ((member_boxes
+  JOIN members ON (member_boxes.member_id = members.member_id))
+  LEFT JOIN status ON (members.member_status = status.status_id))
+WHERE (member_boxes.state = 10)
+   AND members.member_status = 5
+
+UNION ALL
+
+SELECT
+   -(1) AS member_box_id,
+   count(*) AS user_id,
+   '' AS member_name,
+   '' AS bought_date,
+   'In Use' AS box_status,
+   status.title AS member_status
+FROM ((member_boxes
+  JOIN members ON (member_boxes.member_id = members.member_id))
+  LEFT JOIN status ON (members.member_status = status.status_id))
+WHERE (member_boxes.state = 10)
+   AND members.member_status = 6
+
+UNION ALL
+
 SELECT
    member_boxes.member_box_id AS member_box_id,
    member_boxes.member_id AS member_id,

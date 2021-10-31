2d447987d935103e086cf83bb50711ba139b75bc
dpslwk <dps.lwk@gmail.com>
2021-10-31 12:59:12 +0000

    tb_access_logs: add entered_zone_id

--- schema/tb_access_logs.sql
+++ schema/tb_access_logs.sql
@@ -5,6 +5,7 @@ CREATE TABLE `access_logs` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned DEFAULT NULL,
   `door_id` int(10) unsigned DEFAULT NULL,
+  `entered_zone_id` int(11) DEFAULT NULL,
   `access_time` datetime NOT NULL,
   `rfid_serial` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `pin` varchar(12) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -14,6 +15,8 @@ CREATE TABLE `access_logs` (
   KEY `IDX_656A05AA76ED395` (`user_id`),
   KEY `IDX_656A05A58639EAE` (`door_id`),
   KEY `access_time_index` (`access_time`),
+  KEY `IDX_656A05A35D9DA3D` (`entered_zone_id`),
+  CONSTRAINT `FK_656A05A35D9DA3D` FOREIGN KEY (`entered_zone_id`) REFERENCES `zones` (`id`),
   CONSTRAINT `FK_656A05A58639EAE` FOREIGN KEY (`door_id`) REFERENCES `doors` (`id`),
   CONSTRAINT `FK_656A05AA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

067d018a0d000ae43d7c1b11d7240a43d5ae3f58
dpslwk <dps.lwk@gmail.com>
2023-01-02 14:42:42 +0000

    vw_low_last_payment_amount: amount_joint_adjusted divide by zero fix

--- views/05_vw_low_last_payment_amount.sql
+++ views/05_vw_low_last_payment_amount.sql
@@ -6,7 +6,7 @@ SELECT
   bt.amount AS amount,
   TRUNCATE(bt.amount / 100, 2) AS amount_pounds,
   (SELECT count(*) FROM user u WHERE u.account_id = bt.account_id) AS joint_count,
-  TRUNCATE(bt.amount / (SELECT count(*) FROM user u WHERE u.account_id = bt.account_id), 0) AS amount_joint_adjusted
+  TRUNCATE(bt.amount / GREATEST(1, (SELECT count(*) FROM user u WHERE u.account_id = bt.account_id)), 0) AS amount_joint_adjusted
 FROM ((bank_transactions bt
   LEFT JOIN vw_low_last_payment_date lpd ON (bt.account_id = lpd.account_id))
   LEFT JOIN accounts a ON (bt.account_id = a.id))
